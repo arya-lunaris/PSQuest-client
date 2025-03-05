873d5ac2958c37fc438f6f40235df00107f0ff16
Arya Ram <arya_ram@outlook.com>
2025-03-05 23:02:57 +0000

    updated base urls to correct path for deployment

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { setToken, getToken } from "../utils/auth";
 
-const BASE_URL = 'http://127.0.0.1:8000/auth';
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/auth`;
 
 export const signup = async (formData) => {
     try {
